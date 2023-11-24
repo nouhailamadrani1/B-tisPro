ab79d1a1f6a52a0eb10f603f9bd95df8d1786c82
jesakim <jesakim1@gmail.com>
2023-11-24 09:31:12 +0100

    adding RentalRequestService methods

--- src/main/java/com/rentalhive/rentalhive/service/RentalRequestService.java
+++ src/main/java/com/rentalhive/rentalhive/service/RentalRequestService.java
@@ -1,7 +1,36 @@
 package com.rentalhive.rentalhive.service;
 
+import com.rentalhive.rentalhive.model.RentalRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.rentalhive.rentalhive.repository.RentalRequestRepository;
+
+import java.util.List;
 
 @Service
 public class RentalRequestService {
+
+    @Autowired
+    private RentalRequestRepository rentalRequestRepository;
+
+    private List<RentalRequest> getAllRentalRequest() {
+        return rentalRequestRepository.findAll();
+    }
+
+    private RentalRequest getRentalRequestById(int id) {
+        return rentalRequestRepository.findById(id).orElse(null);
+    }
+
+    private RentalRequest addRentalRequest(RentalRequest rentalRequest) {
+        return rentalRequestRepository.save(rentalRequest);
+    }
+
+    private RentalRequest updateRentalRequest(int id,RentalRequest rentalRequest) {
+        rentalRequest.setId(id);
+        return rentalRequestRepository.save(rentalRequest);
+    }
+
+    private void deleteRentalRequest(int id) {
+        rentalRequestRepository.deleteById(id);
+    }
 }
