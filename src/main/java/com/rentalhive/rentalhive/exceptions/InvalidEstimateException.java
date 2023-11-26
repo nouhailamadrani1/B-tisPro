3fd2351c5376fc272b0fb4b7e446b9bc11fa0659
Salah <salaheddinehanaoui@gmail.com>
2023-11-26 13:08:31 +0100

    bug where rental_request_id and admin_id is sent null when we call addEstimate in the EstimateService

---
+++ src/main/java/com/rentalhive/rentalhive/exceptions/InvalidEstimateException.java
@@ -0,0 +1,7 @@
+package com.rentalhive.rentalhive.exceptions;
+
+public class InvalidEstimateException extends RuntimeException{
+    public InvalidEstimateException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/rentalhive/rentalhive/service/EstimateService.java
+++ src/main/java/com/rentalhive/rentalhive/service/EstimateService.java
@@ -1,10 +1,17 @@
 package com.rentalhive.rentalhive.service;
 
+import com.rentalhive.rentalhive.exceptions.InvalidEstimateException;
 import com.rentalhive.rentalhive.model.Estimate;
+import com.rentalhive.rentalhive.model.EstimateStatus;
+import com.rentalhive.rentalhive.model.RentalRequest;
 import com.rentalhive.rentalhive.repository.EstimateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +20,8 @@ public class EstimateService {
 
     @Autowired
     private EstimateRepository estimateRepository;
+    @Autowired
+    private RentalRequestService rentalRequestService;
 
     public List<Estimate> getAllEstimates() {
         return (List<Estimate>) estimateRepository.findAll();
@@ -23,6 +32,11 @@ public class EstimateService {
     }
 
     public Estimate addEstimate(Estimate estimate) {
+        // Set estimateStatus to "Pending"
+        estimate.setEstimateStatus(EstimateStatus.Pending);
+        // Calculate estimatedCost based on rental request start and end dates
+//        calculateEstimatedCost(estimate);
+
         return estimateRepository.save(estimate);
     }
 
@@ -34,4 +48,29 @@ public class EstimateService {
     public void deleteEstimate(int id) {
         estimateRepository.deleteById(id);
     }
+
+    private void calculateEstimatedCost(Estimate estimate) {
+        RentalRequest rentalRequest = rentalRequestService.getRentalRequestById(estimate.getRentalRequest().getId());
+
+        if (rentalRequest == null || rentalRequest.getStart_date() == null || rentalRequest.getEnd_date() == null) {
+            throw new InvalidEstimateException("Cannot calculate estimated cost. Rental request data is missing or invalid.");
+        }
+
+        Date startDate = rentalRequest.getStart_date();
+        Date endDate = rentalRequest.getEnd_date();
+
+        // Assuming Equipment has a price property
+        Double equipmentPrice = estimate.getRentalRequest().getEquipment().getPrice();
+
+        // Convert Date objects to Instant (representing a point in time)
+        Instant startInstant = startDate.toInstant();
+        Instant endInstant = endDate.toInstant();
+
+        // Calculate the duration in days
+        long days = Duration.between(startInstant, endInstant).toDays();
+        Double estimatedCost = days * equipmentPrice;
+
+        estimate.setEstimatedCost(estimatedCost);
+    }
+
 }
