99ea0a21c39750f7e1de8666c933ca37865d59d5
nouhailamadrani1 <nouhailamadrani2020@gmail.com>
2023-11-27 10:48:12 +0100

    Contract getAllApprovedEstimatesForClient

--- src/main/java/com/rentalhive/rentalhive/controller/ContractController.java
+++ src/main/java/com/rentalhive/rentalhive/controller/ContractController.java
@@ -1,8 +1,11 @@
 package com.rentalhive.rentalhive.controller;
+import com.rentalhive.rentalhive.model.Estimate;
 import com.rentalhive.rentalhive.service.ContractService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/estimates")
@@ -15,6 +18,13 @@ public class ContractController {
     public ResponseEntity<String> getContract(@PathVariable int id) {
         return ResponseEntity.ok(contractService.checkEstimateStatusUpdate(id));
     }
+
+    @GetMapping("/getAllApprovedEstimatesForClient/{id}")
+    public ResponseEntity<List<Estimate>> getAllApprovedEstimatesForClient(@PathVariable int id) {
+        List<Estimate> approvedEstimates = contractService.getAllApprovedEstimatesForClient(id);
+        return ResponseEntity.ok(approvedEstimates);
+    }
+
 }
 
 
--- src/main/java/com/rentalhive/rentalhive/service/ContractService.java
+++ src/main/java/com/rentalhive/rentalhive/service/ContractService.java
@@ -2,10 +2,14 @@ package com.rentalhive.rentalhive.service;
 
 import com.rentalhive.rentalhive.model.Estimate;
 import com.rentalhive.rentalhive.model.EstimateStatus;
+import com.rentalhive.rentalhive.model.RentalRequest;
 import com.rentalhive.rentalhive.repository.ContractRepository;
+import com.rentalhive.rentalhive.repository.RentalRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -13,6 +17,8 @@ public class ContractService {
 
     @Autowired
     private ContractRepository contractRepository;
+    @Autowired
+    private RentalRequestRepository rentalRequestRepository;
 
     public String checkEstimateStatusUpdate(int id) {
         Estimate estimate = contractRepository.findById(id);
@@ -23,7 +29,22 @@ public class ContractService {
             return "Estimate not found or not approved";
         }
     }
+    public List<Estimate> getAllApprovedEstimatesForClient(int clientId) {
+        List<Estimate> approvedEstimates = new ArrayList<>();
 
+        List<RentalRequest> rentalRequests = rentalRequestRepository.findRentalRequestsByClientId(clientId);
+
+        for (RentalRequest rentalRequest : rentalRequests) {
+            List<Estimate> estimates = rentalRequest.getEstimates();
+            for (Estimate estimate : estimates) {
+                if (estimate.getEstimateStatus() == EstimateStatus.Approved) {
+                    approvedEstimates.add(estimate);
+                }
+            }
+        }
+
+        return approvedEstimates;
+    }
     private String generateContract(Estimate estimate) {
         return "Contract {" +
                 "Cost=" + estimate.getEstimatedCost() +
